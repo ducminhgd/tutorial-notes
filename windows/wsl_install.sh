0ec2a97df199cedf84284c86c23e9dd2394ff4ae
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2025-08-06 21:44:16 +0700

    Update wsl_install.sh

--- windows/wsl_install.sh
+++ windows/wsl_install.sh
@@ -1,17 +1,24 @@
+# this file is updated on 2025-08-06
+
+# Update OS
 apt-get update
-apt-get install -y figlet fonts-powerline python python3
+apt-get -y install zsh git gcc make software-properties-common
 
-echo "SET DEFAULT PYTHON 3.8"
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
-apt-get install -y python3-pip zsh pipenv
-
-echo "INSTALL DEV TOOLS"
-apt-get install -y redis-tools libmysqlclient-dev
-
-echo "INSTALL OH MY ZSH"
+# Install Oh-My-Zsh and setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+## Spaceship prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-echo "INSTALL DIRENV"
-curl -sfL https://direnv.net/install.sh | bash
-echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
-echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+# Install Python
+add-apt-repository ppa:deadsnakes/ppa -y
+apt-get update
+apt-get install python3.14 python3-pip -y
+pip install -U pip uv --break-system-packages
+
+# Install Go
+sudo apt-get install -y bison
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)\
+gvm install go1.24.5 -B
+gvm use go1.24.5 --default
+mkdir -p ~/go/src/github/ducminhgd
